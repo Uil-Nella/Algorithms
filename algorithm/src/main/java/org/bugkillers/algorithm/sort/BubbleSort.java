86dfd7fc7f08785b437ced867026a2fc8f7a4d51
liuxy <291462491@qq.com>
2015-02-05 22:14:16 +0800

    fix bubble sort.
    Change-Id: Ia9bbcffe41f2ec2f5a55165d26600b3f0533a416
    Signed-off-by: liuxy <291462491@qq.com>

--- algorithm/src/main/java/org/bugkillers/algorithm/sort/BubbleSort.java
+++ algorithm/src/main/java/org/bugkillers/algorithm/sort/BubbleSort.java
@@ -3,28 +3,37 @@ package org.bugkillers.algorithm.sort;
 import java.util.Arrays;
 
 /**
- * ð������
+ * 冒泡排序
  * <p/>
+ * 冒泡排序（Bubble Sort）是一种简单的排序算法。它重复地走访过要排序的数列，一次比较两个元素，如果他们的顺序错误就把他们交换过来。走访数列的工作是重复地进行直到没有再需要交换，也就是说该数列已经排序完成。这个算法的名字由来是因为越小的元素会经由交换慢慢“浮”到数列的顶端。
+ * 冒泡排序算法的运作如下:
+ * 比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+ * 对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。在这一点，最后的元素应该会是最大的数。
+ * 针对所有的元素重复以上的步骤，除了最后一个。
+ * 持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
  * <p>
- * @author ������
+ * @author 刘新宇
  *
  * <p>
- * @date 2015��2��5�� ����9:56:54
+ * @date 2015年2月5日 下午9:56:54
  * <p>
  * @version 0.0.1
  */
 public class BubbleSort {
 
+	/**
+	 * 时间复杂度是O（n²）
+	 */
 	private static void bubble() {
 		Integer array[] = { 97, 9, 15, 87, 109, 90, 99, 100 };
-		// ���ֻ��n-1��ѭ��
+		// 最多只做n-1次循环
 		for (int i = 0; i < array.length - 1; i++) {
-			// ��Χ����С��ǰ������Ѿ���С�ˣ�����Ҫ�ٴ�ð��
+			// 范围逐步缩小，两两交换比较
 			for (int j = 0; j < array.length - i - 1; j++) {
-				if (array[i] < array[j]) {
-					int temp = array[i];
-					array[i] = array[j];
-					array[j] = temp;
+				if (array[j] > array[j+1]) {
+					int temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
 				}
 			}
 		}
